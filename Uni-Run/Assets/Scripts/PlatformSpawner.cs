776da3d0d5d1cd844ac8b7c3e68f8f7d95854cbc
LeeYongWoo <48894645+yongwoo5057@users.noreply.github.com>
2020-05-02 17:00:59 +0900

    First Comment

---
+++ Uni-Run/Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour {
+    public GameObject platformPrefab; 
+    public int count = 3; 
+
+    public float timeBetSpawnMin = 1.25f; 
+    public float timeBetSpawnMax = 2.25f; 
+    private float timeBetSpawn;
+
+    public float yMin = -3.5f; 
+    public float yMax = 1.5f; 
+    private float xPos = 20f; 
+
+    private GameObject[] platforms; 
+    private int currentIndex = 0; 
+
+    private Vector2 poolPosition = new Vector2(0, -20);
+    private float lastSpawnTime; 
+
+    
+    void Start() {
+        platforms = new GameObject[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            platforms[i] = Instantiate(platformPrefab, poolPosition, Quaternion.identity);
+        }
+
+        lastSpawnTime = 0f;
+        timeBetSpawn = 0f;
+    }
+
+    void Update() {
+        if (GameManager.instance.isGameover)
+        {
+            return;
+        }
+
+        if (Time.time >= lastSpawnTime + timeBetSpawn)
+        {
+            lastSpawnTime = Time.time;
+
+            timeBetSpawn = Random.Range(timeBetSpawnMin, timeBetSpawnMax);
+
+            float yPos = Random.Range(yMin, yMax);
+
+            platforms[currentIndex].SetActive(false);
+            platforms[currentIndex].SetActive(true);
+
+            platforms[currentIndex].transform.position = new Vector2(xPos, yPos);
+            currentIndex++;
+
+            if (currentIndex >= count)
+            {
+                currentIndex = 0;
+            }
+        }
+    }
+}
---
+++ Uni-Run/Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour {
+    public float speed = 10f; 
+
+    private void Update() {
+        if (!GameManager.instance.isGameover)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+    }
+}
